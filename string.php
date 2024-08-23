60c133a1c39785af122258672547d2d0863373dc
Rony Hossen <49621445+devsrony@users.noreply.github.com>
2024-08-23 23:48:47 +0600

    Add files via upload

---
+++ string.php
@@ -0,0 +1,23 @@
+<?php
+
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+function countVowels($string)
+{
+  $vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  $count = 0;
+
+  for ($i = 0; $i < strlen($string); $i++) {
+    if (in_array($string[$i], $vowels)) {
+      $count++;
+    }
+  }
+
+  return $count;
+}
+
+foreach ($strings as $str) {
+  $vowelCount = countVowels($str);
+  $reversedString = strrev($str);
+  echo "Original String: $str, Vowel Count: $vowelCount, Reversed String: $reversedString\n";
+}
